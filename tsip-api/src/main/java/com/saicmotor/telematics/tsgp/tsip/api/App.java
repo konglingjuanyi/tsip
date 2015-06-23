fdf8ca08f7034442d5825bf747b2e9d32a7ddc0d
xunji <zhangjizhe@aliyun.com>
2015-06-23 13:05:28 +0800

    init

---
+++ tsip-api/src/main/java/com/saicmotor/telematics/tsgp/tsip/api/App.java
@@ -0,0 +1,13 @@
+package com.saicmotor.telematics.tsgp.tsip.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
