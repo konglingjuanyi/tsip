32eb420ef2fb8eb6fc8db32205903e1257987ce5
jack.xu <v_duminghai@saicmotor.com>
2015-07-28 16:43:05 +0800

    tsip code review

--- tsip-otamsghandler/src/main/java/com/saicmotor/telematics/tsgp/tsip/otamsghandler/calldubbo/common/RoadbookServiceEnum.java
+++ tsip-otamsghandler/src/main/java/com/saicmotor/telematics/tsgp/tsip/otamsghandler/calldubbo/common/RoadbookServiceEnum.java
@@ -4,22 +4,24 @@ package com.saicmotor.telematics.tsgp.tsip.otamsghandler.calldubbo.common;
  * Created by Administrator on 2015/7/28.
  */
 public enum RoadbookServiceEnum {
-    QUERY("481","路书搜索"),
-    DETAIL("482","路书详情获取"),
-    COMMENTRECEIVE("483","路书评论信息获取"),
-    ADDDELETECOMMENT("484","路书评论信息添加和删除"),
-    SENDTOCAR("485","路书下发"),
-    FAVORITEQUERY("486","路书收藏列表获取"),
-    ADDTOFAVORITE("487","路书收藏添加"),
-    DELETEFAVORITE("488","路书收藏删除"),
-    WEATHERINFOMATION("2F4","路书沿途天气预报查询"),
-    DETAILBATCH("489","路书详情批量获取");
+    QUERY("481","641","路书搜索"),
+    DETAIL("482","642","路书详情获取"),
+    COMMENTRECEIVE("483","643","路书评论信息获取"),
+    ADDDELETECOMMENT("484","644","路书评论信息添加和删除"),
+    SENDTOCAR("485","648","路书下发"),
+    FAVORITEQUERY("486","645","路书收藏列表获取"),
+    ADDTOFAVORITE("487","646","路书收藏添加"),
+    DELETEFAVORITE("488","647","路书收藏删除"),
+    WEATHERINFOMATION("2F4","633","路书沿途天气预报查询"),
+    DETAILBATCH("489","649","路书详情批量获取");
 
     private  String aid;
+    private String sid;
     private String info;
 
-    RoadbookServiceEnum(String aid,String info) {
+    RoadbookServiceEnum(String aid,String sid,String info) {
         this.aid = aid;
+        this.sid = sid;
         this.info = info;
     }
 
@@ -29,6 +31,10 @@ public enum RoadbookServiceEnum {
 
     public String getInfo() {
         return info;
+    }
+
+    public String getSid() {
+        return sid;
     }
 
     public static void main(String[] args) {
